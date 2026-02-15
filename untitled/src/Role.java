00cd4dae00294838111a248d1d5eccbfd1575400
TaaPls <261738523+TaaPls@users.noreply.github.com>
2026-02-16 00:44:51 +0700

    Add Role class

---
+++ untitled/src/Role.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class Role {
+    String id;
+    String name;
+    String description;
+    Set<Permission> permissions;
+
+    public Role(String name) {
+        this.id = String.valueOf(UUID.fromString(name));
+        this.name = name;
+        permissions = new HashSet<>();
+    }
+
+    public void addPermission(Permission permission) {
+        permissions.add(permission);
+    }
+    public void removePermission(Permission permission) {
+        permissions.remove(permission);
+    }
+    public boolean hasPermission(Permission permission) {
+        return permissions.contains(permission);
+    }
+    public boolean hasPermission(String permissionName, String resource) {
+        for (Permission permission : permissions) {
+            if (permission.matches(permissionName, resource))
+                return true;
+        }
+        return false;
+    }
+    String format() {
+        StringBuilder permissionString = new StringBuilder();
+        for (Permission permission : permissions)
+            permissionString.append("- ").append(permission.format()).append("\n");
+        return "Role: "+name+" [ID: "+id+"]\n"+"Description: "
+                +description+"\nPermissions ("+permissions.size()
+                +")\n"+permissionString;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(id, role.id);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+    @Override
+    public String toString() {
+        return format();
+    }
+}
