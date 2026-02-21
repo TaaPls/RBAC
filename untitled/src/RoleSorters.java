1104e4be05501e1d614f832b3357b335f48432c4
TaaPls <261738523+TaaPls@users.noreply.github.com>
2026-02-22 00:57:17 +0700

    Add comparators

---
+++ untitled/src/RoleSorters.java
@@ -0,0 +1,10 @@
+import java.util.Comparator;
+
+public class RoleSorters {
+    public static Comparator<Role> byName() {
+        return (o1, o2) -> o1.name.compareToIgnoreCase(o2.name);
+    }
+    public static Comparator<Role> byPermissionCount() {
+        return Comparator.comparingInt(o -> o.permissions.size());
+    }
+}
---
+++ untitled/src/UserSorters.java
@@ -0,0 +1,13 @@
+import java.util.Comparator;
+
+public class UserSorters {
+    public static Comparator<User> byUsername() {
+        return (o1, o2) -> o1.username().compareToIgnoreCase(o2.username());
+    }
+    public static Comparator<User> byFullName() {
+        return (o1, o2) -> o1.fullName().compareToIgnoreCase(o2.fullName());
+    }
+    public static Comparator<User> byEmail() {
+        return (o1, o2) -> o1.email().compareToIgnoreCase(o2.email());
+    }
+}
