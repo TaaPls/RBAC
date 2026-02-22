87c16120047dfb557f48fabadd1addb0a3d501a6
TaaPls <261738523+TaaPls@users.noreply.github.com>
2026-02-23 04:12:30 +0700

    Add Assignment manager tests

--- app/src/main/java/rbac/TemporaryAssignment.java
+++ app/src/main/java/rbac/TemporaryAssignment.java
@@ -3,7 +3,7 @@ package rbac;
 import java.time.LocalDate;
 
 public class TemporaryAssignment extends AbstractRoleAssignment {
-    String expiresAt;
+    String expiresAt = String.valueOf(LocalDate.now().plusMonths(1));
     boolean autoRenew;
 
     public TemporaryAssignment(User user, Role role, AssignmentMetadata metadata) {
@@ -14,7 +14,9 @@ public class TemporaryAssignment extends AbstractRoleAssignment {
         expiresAt = newExpirationDate;
     }
     boolean isExpired() {
-        return LocalDate.now().isBefore(LocalDate.parse(expiresAt));
+        return LocalDate.now().isAfter(LocalDate.parse(expiresAt));
+        //System.out.println(LocalDate.now());
+        //return true;
     }
     String getTimeRemaining() {
         return LocalDate.now().datesUntil(LocalDate.parse(expiresAt)).toString();
