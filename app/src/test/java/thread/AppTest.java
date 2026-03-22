b768071b481ccd9f1963a4c613bc501fa91a35f3
TaaPls <261738523+TaaPls@users.noreply.github.com>
2026-03-22 22:40:59 +0700

    Add workflows

---
+++ app/src/test/java/thread/AppTest.java
@@ -0,0 +1,10 @@
+package thread;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    @Test void test() {
+        assertTrue(true);
+    }
+}
